e8e90229ae83899b2ce14bdd320c68d0fefc5e35
VullnetJ <v.jata24@gmail.com>
2020-08-25 00:08:19 +0300

    Solution to the exercise

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,32 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
-
+## The first step is to create an input as a matrix for the vaiable makeCasheMatrix
+## I changed the "m" to "a"
+## The "mean' i changed to "inv"
 makeCacheMatrix <- function(x = matrix()) {
-
+  a <- NULL
+  set <- function(y) {
+    x <<- y
+    a <<- NULL
+  }
+  get <- function()x
+  setinv <- function(inv) a <<- inv
+  getinv <- function() a
+  list(set = set, get = get,
+       setinv = setinv,
+       getinv = getinv)
 }
 
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
+## The same thing is done in the casheinv as well, by replacing "m" to "a" and then "mean" to "inv"
+cacheinv <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  a <- x$getinv()
+  if(!is.null(a)) {
+    message("getting cached data")
+    return(a)
+  }
+  dat <- x$get()
+  a <- solve(dat, ...)
+  x$setinv(a)
+  a
 }
